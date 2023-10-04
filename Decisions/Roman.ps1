6ea3d9dc1011dd13faf91a290f3547f1a2c841cb
Richard Borden <bordenra@outlook.com>
2023-10-04 10:41:25 -0300

    In class exercise

---
+++ Decisions/Roman.ps1
@@ -0,0 +1,47 @@
+#ROMAN NUMERAL THING
+
+[int]$Number = Read-Host "Enter a number between 1-10"
+
+#If number is 1, output I, and so on
+
+if ($Number -eq 1) {
+    Write-Host "I" 
+}
+
+if ($Number -eq 2) {
+    Write-Host "II" 
+}
+
+if ($Number -eq 3) {
+    Write-Host "III" 
+}
+
+if ($Number -eq 4) {
+    Write-Host "IV" 
+}
+
+if ($Number -eq 5) {
+    Write-Host "V" 
+}
+
+if ($Number -eq 6) {
+    Write-Host "VI" 
+}
+
+if ($Number -eq 7) {
+    Write-Host "VII" 
+}
+
+if ($Number -eq 8) {
+    Write-Host "VIII" 
+}
+
+if ($Number -eq 9) {
+    Write-Host "IX" 
+}
+
+if ($Number -eq 10) {
+    Write-Host "X" 
+}
+
+Write-Host "Thanks for playing."
---
+++ hello.ps1
@@ -0,0 +1,2 @@
+Write-Host "MUSTARD POWER" -BackgroundColor Yellow
+Read-Host "Enter a new username: "
